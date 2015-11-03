09ea58b90f7c90f69c2de04d9541bdd77817312d
Jignasa Patel <jignasa.patel@gmail.com>
2015-11-03 16:13:28 -0500

    New Property for Middlename

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -21,15 +21,10 @@ namespace Sample
     public class Customer
     {
 
-        private int _id;
         private string _firstname;
         private string _lastname;
+        private string _middlename;
 
-        public int Id
-        {
-            get { return _id; }
-            set { _id = value; }
-        }
         public string FirstName
         {
             get { return _firstname; }
@@ -39,6 +34,11 @@ namespace Sample
         {
             get { return _lastname; }
             set { _lastname = value; }
+        }
+        public string MiddleName
+        {
+            get { return _middlename; }
+            set { _middlename = value; }
         }
         public string GetFullName()
         {
